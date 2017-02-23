48da6d9fabfceafe3e097c7cc65153314d1d9c3d
maksim <maksim.lin@foxmail.com>
2017-02-23 11:16:37 +0800

    bug 修复
    bug 修复

--- src/Ds/gemo/Pageslider.js
+++ src/Ds/gemo/Pageslider.js
@@ -1,9 +1,5 @@
 /**
-<<<<<<< HEAD
- * @class Ds.gemo.Pageslider
-=======
  * @class Ds.gemo.PageSlider
->>>>>>> master
  * @classdesc:页面平滑滚动
  *  事件 start move end
     start
@@ -26,21 +22,12 @@
 
     window.Ds = window.Ds || {};
     window.Ds.gemo = window.Ds.gemo || {};
-<<<<<<< HEAD
-    window.Ds.gemo.Pageslider = Pageslider;
-
-    function Pageslider(_scenes, _opt) {
-        Ds.Extend(this, new Ds.EventDispatcher());
-        var $scenes = $(_scenes),
-            _self = this;
-=======
     window.Ds.gemo.PageSlider = PageSlider;
 
     function PageSlider(_scenes, _opt) {
         Ds.Extend(this, new Ds.EventDispatcher());
         var $scenes = $(_scenes),
         _self = this;
->>>>>>> master
         // log($scenes)
         //初始化一些参数
         var startY = 0, //开始Y
@@ -71,12 +58,8 @@
             var _targetTouches = event.targetTouches || event.originalEvent.targetTouches;
             var touch = _targetTouches[0];
             var target = $(event.target);
-<<<<<<< HEAD
-            // event.preventDefault();
-=======
             if(_self.capture)return;
 
->>>>>>> master
             if (!_self.sliding) {
                 _self.capture = true;
                 startY = touch.pageY;
@@ -169,12 +152,8 @@
                         state = 'back';
                     }
                     //如果是横向滑动
-<<<<<<< HEAD
-                } else if (level) {
-=======
                 }
                 else if (level) {
->>>>>>> master
 
                     if (diffX < -limitX) {
                         //上一个
@@ -201,11 +180,8 @@
                     upright: upright,
                     state: state
                 });
-<<<<<<< HEAD
-=======
             }else{
               _self.closeSliding();
->>>>>>> master
             }
         });
         this.openSliding = function() {
