7e322d301f98e5d232031dee78b5ece7e3b5e6b1
maksim <maksim.lin@foxmail.com>
2017-03-09 15:31:35 +0800

    通用ga监测
    通用ga监测

--- src/Ds/gemo/QuickTrack.js
+++ src/Ds/gemo/QuickTrack.js
@@ -42,5 +42,37 @@
       if(!opt_label)return;
       _hmt.push(['_trackEvent', category, action, opt_label, opt_value]);
     };
+    /**
+     * GA虚拟PV
+     * @param {[type]} pageurl [description]
+     */
+    this.GAPV=function(pageurl){
+      if(ga){
+        ga('send', 'pageview',pageurl);
+      }
+    };
+    /**
+     * GA监测事件
+     * @param  {[String]} category  [默认Event【百度描述】要监控的目标的类型名称，通常是同一组目标的名字，比如"视频"、"音乐"、"软件"、"游戏"等等。该项必填，不填、填"-"的事件会被抛弃。]
+     * @param  {[String]} action    [默认Click【百度描述】用户跟目标交互的行为，如"播放"、"暂停"、"下载"等等。该项必填，不填、填"-"的事件会被抛弃。]
+     * @param  {[String]} opt_label [必填【百度描述】事件的一些额外信息，通常可以是歌曲的名称、软件的名称、链接的名称等等。该项选填，不填、填"-"代表此项为空。。]
+     * @param  {[String]} opt_value [默认one【百度描述】事件的一些数值信息，比如权重、时长、价格等等，在报表中可以看到其平均值等数据。该项可选。]
+     * @return {[type]}
+     */
+    this.GAEvent=function( opt_label, opt_value,category, action){
+      if(!category)category='Event';
+      if(!action)action='Click';
+      if(!opt_value)opt_value='one';
+      if(!opt_label)return;
+      if(ga){
+        ga('send', {
+          hitType: 'event',
+          eventCategory: category,
+          eventAction: action,
+          eventLabel: opt_label,
+          eventValue:opt_value,
+        });
+      }
+    };
   }
 })();
