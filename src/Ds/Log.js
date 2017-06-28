82c5b8b881e1b102e826345263c034ea9cabb773
林秋 <maksim.lin@foxmail.com>
2017-06-28 11:08:50 +0800

    update

--- src/Ds/Log.js
+++ src/Ds/Log.js
@@ -4,7 +4,7 @@
  * @class Log
  * @extends 无
  * @author: maksim email:maksim.lin@foxmail.com
- * @copyright:   Ds是累积平时项目工作的经验代码库，不属于职位任务与项目的内容。里面代码大部分理念来至曾经flash 前端时代，尽力减小类之间耦合，通过webpack按需request使用。Ds库里内容多来至网络与参考其他开源代码库。Ds库也开源开放，随意使用在所属的职位任务与项目中。
+ * @copyright:
  * @constructor
  **/
 (function(window) {
