4db0155f22fd4c2bebde15b3b98c55f613a3cf08
林秋 <maksim.lin@foxmail.com>
2017-09-22 14:20:00 +0800

    update

--- build/docServer.js
+++ build/docServer.js
@@ -7,7 +7,6 @@ var copy = require('gulp-copy'); //拷贝
 var watch = require('gulp-watch'); //观察者
 var browserSync = require('browser-sync').create(); //自动刷新调试
 
-
 //启动文档编辑调试服务器
 gulp.task('server',function() {
     // 启动browserSync服务
