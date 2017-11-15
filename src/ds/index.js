72e256f2db6fcb2de6b13699d3092102df168442
林秋 <maksim.lin@foxmail.com>
2017-11-15 15:05:51 +0800

    remove

--- src/ds/index.js
+++
@@ -1,4 +0,0 @@
-var root = (typeof window !== 'undefined' ? window : (typeof process === 'object' && typeof require === 'function' && typeof global === 'object') ? global : this);
-
-/** @namespace ds */
-var ds = root.ds = root.ds || {};
